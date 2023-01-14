918940596ae97cf0e25ab2ce788560dd4476722e
vasik2014 <122561095+vasik2014@users.noreply.github.com>
2023-01-14 10:20:08 +0100

    Aktualizovat main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,12 +4,22 @@ input.onPinPressed(TouchPin.P0, function () {
 input.onButtonPressed(Button.A, function () {
     postava.move(-1)
 })
+input.onPinPressed(TouchPin.P2, function () {
+    zivoty += -1
+    serial.writeLine("" + (zivoty))
+})
 input.onButtonPressed(Button.B, function () {
     postava.move(1)
 })
 input.onPinPressed(TouchPin.P1, function () {
     postava.change(LedSpriteProperty.Y, 1)
 })
+input.onGesture(Gesture.Shake, function () {
+    zivoty += 1
+    serial.writeLine("" + (zivoty))
+})
+let zivoty = 0
 let postava: game.LedSprite = null
 postava = game.createSprite(2, 2)
-postava.move(0)
+zivoty = 10
+serial.writeLine("" + (zivoty))
